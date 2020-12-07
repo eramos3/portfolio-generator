7ac732d619bbcf419434756ec8929ac658a9572c
eramos3 <cisco@Eders-MacBook-Pro.local>
2020-12-06 17:38:39 -0800

    generated webpage

--- app.js
+++ app.js
@@ -1,18 +1,11 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 
-// Notice the lack of parentheses around the `profileDataArr` parameter?
-const printProfileData = profileDataArr => {
-    // This...
-    for (let i = 0; i < profileDataArr.length; i += 1) {
-      console.log(profileDataArr[i]);
-    }
-  
-    console.log('================');
-  
-    // Is the same as this...
-    profileDataArr.forEach(profileItem => console.log(profileItem));
+const profileDataArgs = process.argv.slice(2);
+const [name, github] = profileDataArgs;
 
-  };
-  
-  printProfileData(profileDataArgs);
+fs.writeFile('index.html', generatePage("jane", janehub), err => {
+    if (err) throw err;
+
+    console.log('Portfolio complete! Check out index.html to see the output!');
+});
