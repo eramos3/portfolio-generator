c1252f12c6a2eb9ffafd07e2becd01660244cf3f
eramos3 <cisco@Eders-MacBook-Pro.local>
2020-12-06 17:46:44 -0800

    generated webpage

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const generatePage = require('./src/page-template.js');
 const profileDataArgs = process.argv.slice(2);
 const [name, github] = profileDataArgs;
 
-fs.writeFile('index.html', generatePage("jane", janehub), err => {
+fs.writeFile('index.html', generatePage(name, github), err => {
     if (err) throw err;
 
     console.log('Portfolio complete! Check out index.html to see the output!');
