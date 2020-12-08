87a401a4727d30f64bb2983731185d086844b4d9
eramos3 <cisco@Eders-MacBook-Pro.local>
2020-12-07 17:10:46 -0800

    modified html

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const inquirer = require('inquirer');
-
-// const fs = require('fs');
-// const generatePage = require('./src/page-template');
+const fs = require('fs');
+const generatePage = require('./src/page-template');
 
 // const pageHTML = generatePage(name, github);
 
@@ -133,5 +132,10 @@ const promptProject = portfolioData => {
 promptUser()
     .then(promptProject)
     .then(portfolioData => {
-        console.log(portfolioData);
+                const pageHTML = generatePage(portfolioData);
+
+        fs.writeFile('./index.html', pageHTML, err => {
+            if (err) throw new Error(err);
+
+        });
     });
